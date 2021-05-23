81a8498accbf6085998c9a81f2a90fa38b88395c
Calandiel <Calandiel@users.noreply.github.com>
2021-05-23 15:54:25 +0200

    Added a check in unmanaged hashset in the expand and rehash function to prevent freeing null pointers

--- Scripts/UnmanagedCollections.cs
+++ Scripts/UnmanagedCollections.cs
@@ -1223,8 +1223,11 @@ namespace Calandiel.Collections
 						Add(k);
 					}
 				}
-				UnsafeUtility.Free((void*)oldPresence, Allocator.Persistent);
-				UnsafeUtility.Free((void*)oldKeys, Allocator.Persistent);
+				if (oldCapacity > 0)
+				{
+					UnsafeUtility.Free((void*)oldPresence, Allocator.Persistent);
+					UnsafeUtility.Free((void*)oldKeys, Allocator.Persistent);
+				}
 			}
 		}
 
